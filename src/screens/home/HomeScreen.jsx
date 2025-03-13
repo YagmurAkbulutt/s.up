4e6d2ee79c8d0e1d1643b98f34bddb1db4359bae
Selin ATMACA <selinatmacayy@gmail.com>
2025-03-13 22:06:05 +0300

    update

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -1,202 +1,130 @@
 import {useState, useEffect, useMemo, useCallback, useRef} from 'react';
 import {
   View,
+  Text,
   Image,
   StyleSheet,
   Modal,
-  RefreshControl,
-  ScrollView,
   FlatList,
+  Dimensions,
 } from 'react-native';
 import MasonryList from 'react-native-masonry-list';
-import {imageData, width} from '../../utils/helpers';
+import {imageData} from '../../utils/helpers';
 import ImageCarousel from '../../components/Home/ImageCarousel';
 import HomeSkeleton from '../../components/Home/HomeSkeleton';
 import Header from '../../components/Home/Header';
 import FullPostScreen from './FullPostScreen';
 import Loader from '../../components/Loader';
 
-const getImageSize = async image => {
-  return {
-    width: Math.floor((width - 30) / 2),
-    height: Math.floor(Math.random() * 100) + 250,
-  };
-};
+const {width, height} = Dimensions.get('window');
 
 const HomeScreen = () => {
   const [formattedImages, setFormattedImages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
-  const [loaderVisible, setLoaderVisible] = useState(false);
-  const [firstRefresh, setFirstRefresh] = useState(true);
-
-  const handleRefresh = () => {
-    setFirstRefresh(false);
-    onRefresh();
-  };
+  const flatListRef = useRef(null);
 
   const loadImages = async () => {
     setLoading(true);
-    const updatedImages = await Promise.all(
-      imageData.map(async item => {
-        const firstImage = item.images[0];
-        const {width, height} = await getImageSize(firstImage);
-
-        return {
-          id: item.id,
-          uri: Image.resolveAssetSource(firstImage).uri,
-          width: width,
-          height: height,
-          images: item.images,
-          isCarousel: item.images.length > 1,
-          username: item.username,
-          description: item.description,
-          tags: item.tags,
-        };
-      }),
-    );
+    const updatedImages = imageData.map(item => ({
+      id: item.id,
+      uri: Image.resolveAssetSource(item.images[0]).uri,
+      width: width,
+      height: height,
+      images: item.images,
+      isCarousel: item.images.length > 1,
+      username: item.username,
+      description: item.description,
+      tags: item.tags,
+    }));
 
     setTimeout(() => {
       setFormattedImages(updatedImages);
       setLoading(false);
       setRefreshing(false);
-    }, 1500);
+    }, 1000);
   };
 
   useEffect(() => {
     loadImages();
   }, []);
 
-  const onRefresh = useCallback(() => {
-    setRefreshing(true);
-    setLoaderVisible(true);
-    loadImages();
-  }, []);
+  const handlePress = item => {
+    console.log('Seçilen resim:', item);
+    setSelectedImage(item);
 
-  const onScroll = event => {
-    const contentOffsetY = event.nativeEvent.contentOffset.y;
-
-    if (contentOffsetY < 100) {
-      setLoaderVisible(true);
-    } else {
-      setLoaderVisible(false);
+    const index = formattedImages.findIndex(img => img.id === item.id);
+    if (flatListRef.current && index !== -1) {
+      setTimeout(() => {
+        flatListRef.current.scrollToIndex({index, animated: false});
+      }, 100);
     }
   };
 
   const customImageComponent = useMemo(
     () => item => {
       const foundItem = formattedImages.find(i => i.uri === item.source.uri);
-
-      const handlePress = (item, index) => {
-        setSelectedImage(item);
-      };
-
       return foundItem ? (
-        <ImageCarousel item={foundItem} onPress={handlePress} />
+        <ImageCarousel
+          item={foundItem}
+          onPress={() => handlePress(foundItem)}
+        />
       ) : null;
     },
     [formattedImages],
   );
 
-  const dataToScroll = formattedImages.map(image => ({
-    username: image.username,
-    description: image.description,
-    tags: image.tags ? image.tags.join(', ') : '',
-  }));
-  const masonryRef = useRef(null);
-  console.log('Modal açıldı, formattedImages:', formattedImages);
-  console.log('Modal açılınca selectedImage:', selectedImage);
-
   return (
     <View style={styles.container}>
       <Header />
-      <>
-        {loading ? (
-          <HomeSkeleton />
-        ) : (
-          <>
-            {refreshing && !firstRefresh ? (
-              <View style={styles.loaderContainer}>
-                <Loader />
-              </View>
-            ) : null}
-
-            <ScrollView
-              nestedScrollEnabled={true}
-              keyboardShouldPersistTaps="handled"
-              scrollEventThrottle={16}
-              onScroll={onScroll}
-              showsVerticalScrollIndicator={false}
-              refreshControl={
-                <RefreshControl
-                  refreshing={refreshing}
-                  onRefresh={handleRefresh}
-                  tintColor="transparent"
-                  colors={['transparent']}
-                />
-              }>
-              <MasonryList
-                images={formattedImages}
-                customImageComponent={customImageComponent}
-                columns={2}
-                spacing={1}
-                showsVerticalScrollIndicator={false}
-                scrollEnabled={false}
-              />
-            </ScrollView>
-          </>
-        )}
-      </>
-      {selectedImage && (
-      //    <Modal
-      //    visible={!!selectedImage}
-      //    animationType="slide"
-      //    onRequestClose={() => setSelectedImage(null)}
-      //  >
-      //    {formattedImages.length > 0 ? (
-      //      <FlatList
-      //        data={formattedImages}
-      //        keyExtractor={(item, index) =>
-      //          item.id ? item.id.toString() : index.toString()
-      //        }
-      //        horizontal
-      //        pagingEnabled
-      //        style={{ flex: 1 }} 
-      //        contentContainerStyle={{ flexGrow: 1 }} 
-      //        initialScrollIndex={Math.max(
-      //          formattedImages.findIndex(img => img.id === selectedImage.id),
-      //          0 
-      //        )}
-      //        getItemLayout={(data, index) => ({
-      //          length: 300, 
-      //          offset: 300 * index,
-      //          index,
-      //        })}
-      //        renderItem={({ item }) => (
-      //          <FullPostScreen
-      //            image={item}
-      //            onClose={() => setSelectedImage(null)}
-      //            extraData={dataToScroll}
-      //          />
-      //        )}
-      //      />
-      //    ) : (
-      //      <Text style={{ textAlign: "center", marginTop: 20 }}>Yükleniyor...</Text>
-      //    )}
-      //  </Modal>
-       <Modal
-       visible={!!selectedImage}
-       animationType="slide"
-       onRequestClose={() => setSelectedImage(null)}>
-       <FullPostScreen
-         image={selectedImage}
-         onClose={() => setSelectedImage(null)}
-         extraData={dataToScroll}
-       />
-     </Modal>
-       
+      {loading ? (
+        <HomeSkeleton />
+      ) : (
+        <MasonryList
+          images={formattedImages}
+          customImageComponent={customImageComponent}
+          columns={2}
+          spacing={1}
+          showsVerticalScrollIndicator={false}
+        />
       )}
+
+      {/* ✅ Çalışan Swipe Destekli Modal */}
+      <Modal
+        visible={!!selectedImage}
+        animationType="slide"
+        onRequestClose={() => setSelectedImage(null)}>
+        <View style={styles.modalContainer}>
+          {formattedImages.length > 0 ? (
+            <FlatList
+              ref={flatListRef}
+              data={formattedImages}
+              keyExtractor={item => item.id.toString()}
+              horizontal
+              pagingEnabled
+              showsHorizontalScrollIndicator={false}
+              initialScrollIndex={
+                formattedImages.findIndex(
+                  img => img.id === selectedImage?.id,
+                ) || 0
+              }
+              getItemLayout={(data, index) => ({
+                length: width,
+                offset: width * index,
+                index,
+              })}
+              renderItem={({item}) => (
+                <View style={styles.imageContainer}>
+                  <Image source={{uri: item.uri}} style={styles.image} />
+                </View>
+              )}
+            />
+          ) : (
+            <Text style={styles.loadingText}>Yükleniyor...</Text>
+          )}
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -208,47 +136,27 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     paddingHorizontal: 5,
-    paddingTop: Platform.OS === 'android' && 5,
+    paddingTop: Platform.OS === 'android' ? 5 : 0,
   },
-  masonry: {
+  modalContainer: {
     flex: 1,
-    paddingHorizontal: 50,
-    paddingVertical: 20,
-  },
-  loader: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    zIndex: 10,
-    alignItems: 'center',
+    backgroundColor: 'black',
     justifyContent: 'center',
-    height: 50,
-  },
-  loaderContainer: {
-    position: 'absolute',
-    top: 10,
-    left: 0,
-    right: 0,
-    zIndex: 100,
     alignItems: 'center',
-    justifyContent: 'center',
-    height: 50,
+  },
+  imageContainer: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  loadingText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 20,
   },
 });
-
-{
-  /*
-  
-  <Modal
-          visible={!!selectedImage}
-          animationType="slide"
-          onRequestClose={() => setSelectedImage(null)}>
-          <FullPostScreen
-            image={selectedImage}
-            onClose={() => setSelectedImage(null)}
-            extraData={dataToScroll}
-          />
-        </Modal>
-  */
-}
